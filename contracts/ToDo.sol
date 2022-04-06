4914fce3b705cc2ba8f418079abcec95c0a68607
VaheAbelyan <abelyan.vahe@gmail.com>
2022-04-06 15:59:47 +0400

    Improved ToDo Contract

--- contracts/ToDo.sol
+++ contracts/ToDo.sol
@@ -1,5 +1,22 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
 
 contract ToDo {
-    
+    struct Task {
+        uint256 id;
+        string name;
+        string content;
+        string status;
+    }
+
+    Task[] public tasks;
+
+    constructor() public {
+        tasks.push(Task({
+            id: 1,
+            name: "Sample Task",
+            content: "This is a sample task",
+            status: "new"
+        }));
+    }
 }
